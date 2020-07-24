7f210616ea5c6bd68a09a3cac50d32864484f525
finga <finga@users.noreply.github.com>
2020-07-24 12:51:40 +0200

    feat: add command line argument parsing
    In order to cope with command line arguments `clap` is used. To stick
    to single sources of truth the meta information used in the help
    message is taken from `Cargo.toml`.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use clap::{App, Arg};
+use std::path::Path;
+use std::process;
+
 mod compile;
 
-fn print_usage() {
-    println!("should print usage")
-}
+use compile::compile_example;
 
 fn main() {
-    print_usage()
+    let args = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS").replace(":", ", ").as_str())
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("compile")
+                .short('c')
+                .long("compile")
+                .value_name("FILE")
+                .about("compile a source file")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    if let Some(source) = args.value_of("compile") {
+        if let Err(e) = compile_example(Path::new(source)) {
+            println!("{}", e);
+            process::exit(1);
+        }
+    }
 }
