07c011147e5b386a65d6ed2aa692711be9e070da
Christian Mösl <christian.moesl@live.at>
2020-08-13 15:43:16 +0200

    refactor: graph generation

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,51 +1,92 @@
-use crate::decode::{Decoder, RiscU};
 use crate::elf::load_file;
 use byteorder::{ByteOrder, LittleEndian};
 use petgraph::graph::NodeIndex;
 use petgraph::Graph;
-use riscv_decode::types::*;
+use riscv_decode::decode;
 use riscv_decode::Instruction;
 use std::path::Path;
+use std::vec::Vec;
 
-type ControlFlowGraph = Graph<Instruction, u8>;
+type ControlFlowGraph = Graph<Instruction, usize>;
 
 struct CfgBuilder {
     graph: ControlFlowGraph,
-    idx: usize,
-    prev_idx: usize,
+    // idx: usize,
+    // prev_idx: usize,
+    // edges_todo: Vec::<(NodeIndex::<u32>, NodeIndex::<u32>, usize)>,
     // is_finished: bool,
 }
 
 impl CfgBuilder {
-    fn new() -> Self {
+    fn new(binary: &[u8]) -> Self {
+        let g = binary
+            .chunks_exact(4)
+            .map(LittleEndian::read_u32)
+            .map(decode)
+            .map(Result::unwrap)
+            .fold(ControlFlowGraph::new(), |mut g, i| {
+                g.add_node(i);
+                g
+            });
+
         CfgBuilder {
-            graph: ControlFlowGraph::new(),
-            idx: 0,
-            prev_idx: 0,
+            graph: g,
+            // idx: 0,
+            // prev_idx: 0,
+            // edges_todo: Vec::<(NodeIndex::<u32>, NodeIndex::<u32>, usize)>::new(),
             // is_finished: false,
         }
     }
 
-    fn build(binary: &[u8]) -> ControlFlowGraph {
-        let mut builder = CfgBuilder::new();
-        let mut pipeline = Decoder::new(&mut builder);
+    fn is_control_flow_instruction(&mut self, idx: NodeIndex) -> bool {
+        let instruction = self.graph[idx];
 
-        let words = binary
-            .chunks_exact(4)
-            .map(LittleEndian::read_u32)
-            .collect::<Vec<u32>>();
-
-        // while !pipeline.next.is_finished {
-        while pipeline.next.idx < words.len() {
-            let instruction = words[pipeline.next.idx];
-
-            pipeline.run(instruction);
+        match instruction {
+            Instruction::Jal(_) => true,
+            Instruction::Jalr(_) => true,
+            Instruction::Beq(_) => true,
+            _ => false,
         }
+    }
+
+    fn compute_trivial_edges(&mut self) {
+        let indices = self.graph.node_indices();
+        let len = indices.len() - 1;
+
+        let edges: Vec<(NodeIndex, NodeIndex, usize)> = indices
+            .take(len)
+            .filter(|i| !self.is_control_flow_instruction(*i))
+            .map(|idx| {
+                (
+                    NodeIndex::new(idx.index()),
+                    NodeIndex::new(idx.index() + 1),
+                    0,
+                )
+            })
+            .collect();
+
+        edges.iter().for_each(|e| {
+            self.graph.add_edge(e.0, e.1, e.2);
+        });
+    }
+
+    fn compute_beq_edges(&mut self) {
+        // let indices = self.graph.node_indices();
+        // let len = indices.len() - 1;
+
+        // let beqs = indices.filter(predicate: P)
+    }
+
+    fn build(binary: &[u8]) -> ControlFlowGraph {
+        let mut builder = CfgBuilder::new(binary);
+
+        builder.compute_trivial_edges();
+        builder.compute_beq_edges();
 
         // TODO: refactor that
         // remove first edge from node 1 to node 1
 
-        pipeline.next.graph.clone()
+        builder.graph
     }
 
     // TODO: only tested with Selfie RISC-U file and relies on that ELF format
@@ -63,73 +104,85 @@ impl CfgBuilder {
         }
     }
 
-    fn step(&mut self, size: usize) {
-        self.prev_idx = self.idx;
-        self.idx += size;
-    }
+    // fn step(&mut self, size: usize) {
+    //     self.prev_idx = self.idx;
+    //     self.idx += size;
+    // }
 
-    fn append_trivial(&mut self, i: Instruction) {
-        let n = self.graph.add_node(i);
-        self.graph.add_edge(NodeIndex::new(self.prev_idx), n, 0);
-        self.step(1);
-    }
+    // fn append_trivial(&mut self, i: Instruction) {
+    //     let n = self.graph.add_node(i);
+    //     self.step(1);
+    // }
 }
 
-impl RiscU for CfgBuilder {
-    fn lui(&mut self, i: UType) {
-        self.append_trivial(Instruction::Lui(i));
-    }
-    fn addi(&mut self, i: IType) {
-        self.append_trivial(Instruction::Addi(i));
-    }
-    fn add(&mut self, i: RType) {
-        self.append_trivial(Instruction::Add(i));
-    }
-    fn sub(&mut self, i: RType) {
-        self.append_trivial(Instruction::Sub(i));
-    }
-    fn mul(&mut self, i: RType) {
-        self.append_trivial(Instruction::Mul(i));
-    }
-    fn divu(&mut self, i: RType) {
-        self.append_trivial(Instruction::Divu(i));
-    }
-    fn remu(&mut self, i: RType) {
-        self.append_trivial(Instruction::Remu(i));
-    }
-    fn sltu(&mut self, i: RType) {
-        self.append_trivial(Instruction::Sltu(i));
-    }
-    fn ld(&mut self, i: IType) {
-        self.append_trivial(Instruction::Ld(i));
-    }
-    fn sd(&mut self, i: SType) {
-        self.append_trivial(Instruction::Sd(i));
-    }
-    fn jal(&mut self, i: JType) {
-        self.append_trivial(Instruction::Jal(i));
-    }
-    fn jalr(&mut self, i: IType) {
-        self.append_trivial(Instruction::Jalr(i));
-    }
-    fn beq(&mut self, i: BType) {
-        let n = self.graph.add_node(Instruction::Beq(i));
-        self.graph.add_edge(NodeIndex::new(self.prev_idx), n, 0);
+// impl RiscU for CfgBuilder {
+//     fn lui(&mut self, i: UType) {
+//         self.append_trivial(Instruction::Lui(i));
+//     }
+//     fn addi(&mut self, i: IType) {
+//         self.append_trivial(Instruction::Addi(i));
+//     }
+//     fn add(&mut self, i: RType) {
+//         self.append_trivial(Instruction::Add(i));
+//     }
+//     fn sub(&mut self, i: RType) {
+//         self.append_trivial(Instruction::Sub(i));
+//     }
+//     fn mul(&mut self, i: RType) {
+//         self.append_trivial(Instruction::Mul(i));
+//     }
+//     fn divu(&mut self, i: RType) {
+//         self.append_trivial(Instruction::Divu(i));
+//     }
+//     fn remu(&mut self, i: RType) {
+//         self.append_trivial(Instruction::Remu(i));
+//     }
+//     fn sltu(&mut self, i: RType) {
+//         self.append_trivial(Instruction::Sltu(i));
+//     }
+//     fn ld(&mut self, i: IType) {
+//         self.append_trivial(Instruction::Ld(i));
+//     }
+//     fn sd(&mut self, i: SType) {
+//         self.append_trivial(Instruction::Sd(i));
+//     }
+//     fn jal(&mut self, i: JType) {
+//         // JAL with ra -> function call
+//         // JAL with zero -> just jump
+//         let n = self.graph.add_node(Instruction::Jal(i));
+//         self.graph.add_edge(NodeIndex::new(self.prev_idx), n, 0);
 
-        // idx * 4 -> PC <=> PC / 4 -> idx
-        if i.imm() / 4 < self.idx as u32 {
-            // while loop
-            self.graph
-                .add_edge(n, NodeIndex::new((i.imm() / 4) as usize), 0);
-            self.step(1);
-        } else {
-            self.step(1);
-        }
-    }
-    fn ecall(&mut self) {
-        self.append_trivial(Instruction::Ecall);
-    }
-}
+//         if i.rd() == 0 {
+//           // jump without saving the link address
+//           let destination = self.idx + i.imm() as usize / 4;
+//           let e = (NodeIndex::new(n.index()), NodeIndex::new(destination), self.idx);
+//           self.edges_todo.push(e);
+//         } else {
+//           // jump to a function
+
+//         }
+//     }
+//     fn jalr(&mut self, i: IType) {
+//         self.append_trivial(Instruction::Jalr(i));
+//     }
+//     fn beq(&mut self, i: BType) {
+//         let n = self.graph.add_node(Instruction::Beq(i));
+//         self.graph.add_edge(NodeIndex::new(self.prev_idx), n, 0);
+
+//         // idx * 4 -> PC <=> PC / 4 -> idx
+//         if (self.idx as u32 + i.imm()) / 4 < self.idx as u32 {
+//             // while loop
+//             self.graph
+//                 .add_edge(n, NodeIndex::new((i.imm() / 4) as usize), 0);
+//             self.step(1);
+//         } else {
+//             self.step(1);
+//         }
+//     }
+//     fn ecall(&mut self) {
+//         self.append_trivial(Instruction::Ecall);
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -137,6 +190,8 @@ mod tests {
     use petgraph::dot::{Config, Dot};
     use serial_test::serial;
     use std::env::current_dir;
+    use std::fs::File;
+    use std::io::prelude::*;
     use std::process::Command;
     use std::string::String;
 
@@ -163,7 +218,19 @@ mod tests {
 
         let graph = CfgBuilder::build_from_file(test_file).unwrap();
 
-        println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+        let dot_graph = Dot::with_config(&graph, &[Config::EdgeNoLabel]);
+
+        let mut f = File::create("tmp-graph.dot").unwrap();
+        f.write_fmt(format_args!("{:?}", dot_graph)).unwrap();
+
+        // let _ = Command::new("dot")
+        //     .arg("-Tpng")
+        //     .arg("tmp-graph.dot")
+        //     .arg("-o")
+        //     .arg("main.png")
+        //     .output();
+
+        // println!("{:?}", ));
 
         // TODO: test more than just this result
         // assert!(result.is_ok());
