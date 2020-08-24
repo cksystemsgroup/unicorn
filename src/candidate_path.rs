9fd31abcdbd0bf01e3a75dbc4a532ef36f1683bb
Alexandru Stirbu-Blagut <astirbu@protonmail.com>
2020-08-24 12:10:40 +0200

    feat: functions for finding candidate path root nodes (WIP)

--- src/candidate_path.rs
+++ src/candidate_path.rs
@@ -3,18 +3,18 @@ use petgraph::graph::NodeIndex;
 use petgraph::visit::EdgeRef;
 use riscv_decode::Instruction;
 
-pub struct CandidatePath {
-    pub root: NodeIndex, // instruction we want to evaluate (exit syscall, division with a potential 0 as divisor, ... )
+pub struct CandidatePath<'a> {
+    pub root: &'a NodeIndex, // instruction we want to evaluate (exit syscall, division with a potential 0 as divisor, ... )
     pub path: ControlFlowGraph, // instructions which connect the root to the leaves
-    pub leaves: Vec<NodeIndex>, // read syscalls
-    pub alternative_roots: Vec<NodeIndex>, // alternative roots for generating new candidate paths of the same control-flow graph (relevant for CandidatePath::next())
-    pub cfg: ControlFlowGraph, // control-flow graph which this candidate path is extracted from
+    pub leaves: Vec<&'a NodeIndex>, // read syscalls
+    pub alternative_roots: Vec<&'a NodeIndex>, // alternative roots for generating new candidate paths of the same control-flow graph (relevant for CandidatePath::next())
+    pub cfg: &'a ControlFlowGraph, // control-flow graph which this candidate path is extracted from
 }
 
 #[allow(dead_code)]
-impl CandidatePath {
+impl<'a> CandidatePath<'a> {
     // may return a reference to a new candidate path using a new root
-    pub fn next(&mut self) -> &mut CandidatePath {
+    pub fn next(&'a mut self) -> &mut CandidatePath {
         self.root = self.alternative_roots.pop().unwrap();
         self.path = ControlFlowGraph::new();
         self.leaves = vec![];
@@ -33,7 +33,50 @@ impl CandidatePath {
     // pub fn compute_initial_candidate_path(graph: &ControlFlowGraph) -> CandidatePath { }
 
     // finds root nodes (exit syscall, division with a potential 0 as divisor, ... ) using a provided control-flow graph
-    // fn find_roots(graph: &ControlFlowGraph) -> Vec<NodeIndex> { }
+    #[allow(dead_code)]
+    fn find_roots(graph: &ControlFlowGraph) -> Vec<NodeIndex> {
+        let mut roots = vec![];
+
+        graph.node_indices().for_each(|idx| {
+            if let Instruction::Ecall = graph[idx] {
+                if let Some(idx) = is_exit_point(graph, idx) {
+                    roots.push(idx);
+                }
+            } else if let Instruction::Divu(_rtype) = graph[idx] {
+                if let Some(idx) = is_exit_point(graph, idx) {
+                    roots.push(idx);
+                }
+            };
+        });
+
+        roots
+    }
+}
+
+#[allow(dead_code)]
+fn is_exit_point(graph: &ControlFlowGraph, idx: NodeIndex) -> Option<NodeIndex> {
+    match graph[idx] {
+        // naive approach of finding division instructions with a divisor of 0
+        // TODO: search for read syscalls, trace those inputs and find divu instructions which deal with read syscall data
+        // -> waiting for better control-flow graph instruction annotations
+        Instruction::Divu(a) => {
+            if a.rs2() == 0 {
+                Some(idx)
+            } else {
+                None
+            }
+        }
+        _ => match graph[NodeIndex::new(idx.index() - 1)] {
+            Instruction::Addi(a) => {
+                if a.imm() == 93 {
+                    Some(idx)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        },
+    }
 }
 
 // extracts a trivial condidate path starting from the very last jump to an ecall (exit)
