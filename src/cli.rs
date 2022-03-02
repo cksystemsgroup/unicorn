48055a07eb3aede44a6a946a312247f8d977f258
Michael Starzinger <michael.starzinger@antforge.org>
2022-03-02 13:18:31 +0100

    chore: remove old code unrelated to unicorn

--- src/cli.rs
+++ src/cli.rs
@@ -1,15 +1,8 @@
 use anyhow::{anyhow, Context, Result};
+use bytesize::ByteSize;
 use clap::{crate_authors, crate_description, crate_version, App, AppSettings, Arg, ArgMatches};
 use const_format::formatcp;
-use lazy_static::lazy_static;
-use monster::{
-    engine::{
-        rarity_simulation::{defaults as rarity_defaults, MeanType},
-        symbolic_execution::defaults as symbolic_defaults,
-    },
-    path_exploration::ExplorationStrategyType,
-    solver::{SmtType, SolverType},
-};
+use monster::solver::SmtType;
 use std::str::FromStr;
 use strum::{EnumString, EnumVariantNames, IntoStaticStr, VariantNames};
 
@@ -23,9 +16,7 @@ pub enum LogLevel {
     Error,
 }
 
-lazy_static! {
-    static ref COPY_INIT_RATIO: String = format!("{}", rarity_defaults::COPY_INIT_RATIO);
-}
+const MEMORY_SIZE: ByteSize = ByteSize(bytesize::MIB);
 
 pub fn args() -> App<'static> {
     App::new("Monster")
@@ -52,82 +43,6 @@ pub fn args() -> App<'static> {
                         .help("Binary file to be disassembled")
                         .takes_value(true)
                         .required(true),
-                ),
-        )
-        .subcommand(
-            App::new("cfg")
-                .about("Generate control flow graph from RISC-U ELF binary")
-                .arg(
-                    Arg::new("input-file")
-                        .help("Source RISC-U binary to be analyzed")
-                        .takes_value(true)
-                        .value_name("FILE")
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("output-file")
-                        .help("Output file to write to")
-                        .short('o')
-                        .long("output-file")
-                        .takes_value(true)
-                        .value_name("FILE")
-                        .default_value("cfg.dot"),
-                )
-                .arg(
-                    Arg::new("distances")
-                        .help("Compute also shortest path distances from exit")
-                        .short('d')
-                        .long("distances"),
-                ),
-        )
-        .subcommand(
-            App::new("execute")
-                .about("Symbolically execute a RISC-U ELF binary")
-                .arg(
-                    Arg::new("input-file")
-                        .help("RISC-U ELF binary to be executed")
-                        .takes_value(true)
-                        .value_name("FILE")
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("solver")
-                        .help("SMT solver")
-                        .short('s')
-                        .long("solver")
-                        .takes_value(true)
-                        .value_name("SOLVER")
-                        .possible_values(SolverType::VARIANTS)
-                        .default_value(SolverType::Monster.into()),
-                )
-                .arg(
-                    Arg::new("max-execution-depth")
-                        .help("Number of instructions, where the path execution will be aborted")
-                        .short('d')
-                        .long("execution-depth")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", symbolic_defaults::MAX_EXECUTION_DEPTH))
-                        .validator(is::<u64>),
-                )
-                .arg(
-                    Arg::new("memory")
-                        .help("Amount of memory to be used per execution context in megabytes [possible_values: 1 .. 1024]")
-                        .short('m')
-                        .long("memory")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", symbolic_defaults::MEMORY_SIZE.0 / bytesize::MIB))
-                        .validator(is_valid_memory_size),
-                )
-                .arg(
-                    Arg::new("strategy")
-                    .help("Path exploration strategy to use when exploring state search space")
-                    .long("strategy")
-                    .takes_value(true)
-                    .value_name("STRATEGY")
-                    .default_value(ExplorationStrategyType::ShortestPaths.into())
-                    .possible_values(ExplorationStrategyType::VARIANTS)
                 ),
         )
         .subcommand(
@@ -170,7 +85,7 @@ pub fn args() -> App<'static> {
                         .long("memory")
                         .takes_value(true)
                         .value_name("NUMBER")
-                        .default_value(formatcp!("{}", symbolic_defaults::MEMORY_SIZE.0 / bytesize::MIB))
+                        .default_value(formatcp!("{}", MEMORY_SIZE.0 / bytesize::MIB))
                         .validator(is_valid_memory_size),
                 )
                 .arg(
@@ -205,137 +120,6 @@ pub fn args() -> App<'static> {
                     .value_name("NUMBER")
                     .validator(is::<usize>),
                 )
-        )
-        .subcommand(
-            App::new("smt")
-                .about("Create an SMT-LIB file for a RISC-U ELF binary")
-                .arg(
-                    Arg::new("input-file")
-                        .help("RISC-U ELF binary to be converted")
-                        .takes_value(true)
-                        .value_name("FILE")
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("max-execution-depth")
-                        .help("Number of instructions, where the path execution will be aborted")
-                        .short('d')
-                        .long("execution-depth")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", symbolic_defaults::MAX_EXECUTION_DEPTH))
-                        .validator(is::<u64>),
-                )
-                .arg(
-                    Arg::new("memory")
-                        .help("Amount of memory to be used per execution context in megabytes [possible_values: 1 .. 1024]")
-                        .short('m')
-                        .long("memory")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", symbolic_defaults::MEMORY_SIZE.0 / bytesize::MIB))
-                        .validator(is_valid_memory_size),
-                )
-                .arg(
-                    Arg::new("strategy")
-                    .help("Path exploration strategy to use when exploring state search space")
-                    .long("strategy")
-                    .takes_value(true)
-                    .value_name("STRATEGY")
-                    .default_value(ExplorationStrategyType::ShortestPaths.into())
-                    .possible_values(ExplorationStrategyType::VARIANTS)
-                )
-                .arg(
-                    Arg::new("smt-type")
-                    .help("Specify a solver type to generate the SMT file for")
-                    .long("smt-type")
-                    .short('t')
-                    .takes_value(true)
-                    .default_value(SmtType::Generic.into())
-                    .possible_values(SmtType::VARIANTS)
-                )
-                .arg(
-                    Arg::new("output-file")
-                    .help("Output path for the generated SMT-LIB file")
-                    .short('o')
-                    .long("out")
-                    .takes_value(true)
-                )
-        )
-        .subcommand(
-            App::new("rarity")
-                .about("Performs rarity simulation on a RISC-U ELF binary")
-                .arg(
-                    Arg::new("input-file")
-                        .help("Source RISC-U binary to be analyzed")
-                        .takes_value(true)
-                        .value_name("FILE")
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("memory")
-                        .help("Amount of memory to be used per execution context in megabytes [possible_values: 1 .. 1024]")
-                        .short('m')
-                        .long("memory")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", rarity_defaults::MEMORY_SIZE.0 / bytesize::MIB))
-                        .validator(is_valid_memory_size),
-                )
-                .arg(
-                    Arg::new("step-size")
-                        .help("Instructions to be executed for each round")
-                        .long("step-size")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", rarity_defaults::STEP_SIZE))
-                        .validator(is::<u64>),
-                )
-                .arg(
-                    Arg::new("states")
-                        .help("Number of distinct states")
-                        .long("states")
-                        .takes_value(true)
-                        .value_name("NUMBER")
-                        .default_value(formatcp!("{}", rarity_defaults::AMOUNT_OF_STATES))
-                        .validator(is::<usize>),
-                )
-                .arg(
-                    Arg::new("selection")
-                    .help("Number of runs to select in every iteration")
-                    .short('s')
-                    .long("selection")
-                    .takes_value(true)
-                    .value_name("NUMBER")
-                    .default_value(formatcp!("{}", rarity_defaults::SELECTION))
-                    .validator(is::<usize>))
-                .arg(
-                    Arg::new("iterations")
-                    .help("Iterations of rarity simulation to run")
-                    .short('i')
-                    .long("iterations")
-                    .takes_value(true)
-                    .value_name("NUMBER")
-                    .default_value(formatcp!("{}", rarity_defaults::ITERATIONS))
-                    .validator(is::<u64>))
-                .arg(
-                    Arg::new("copy-init-ratio")
-                        .help("Determines how much new states are copied instead of started from the beginning")
-                        .long("copy-init-ratio")
-                        .takes_value(true)
-                        .value_name("RATIO")
-                        .default_value(COPY_INIT_RATIO.as_str())
-                        .validator(is_ratio)
-                    )
-                .arg(
-                    Arg::new("mean")
-                    .help("The average to be used for the counts")
-                    .long("mean")
-                    .takes_value(true)
-                    .value_name("MEAN")
-                    .possible_values(MeanType::VARIANTS)
-                    .default_value(rarity_defaults::MEAN_TYPE.into())
-                    )
         )
         .subcommand(
             App::new("qubot")
@@ -429,21 +213,6 @@ fn is_valid_memory_size(v: &str) -> Result<(), String> {
             Err(String::from("memory size has to be in range: 1 - 1024"))
         }
     })
-}
-
-fn is_ratio(v: &str) -> Result<(), String> {
-    let valid_range = 0.0_f64..=1.0f64;
-
-    match v.parse::<f64>() {
-        Ok(ratio) => {
-            if valid_range.contains(&ratio) {
-                Ok(())
-            } else {
-                Err("Expected range between 0.0 and 1.0".to_string())
-            }
-        }
-        Err(err) => Err(err.to_string()),
-    }
 }
 
 #[cfg(test)]
--- src/util.rs
+++ src/util.rs
@@ -14,15 +14,6 @@ macro_rules! time_info {
         result
     }};
 }
-macro_rules! time_debug {
-    ($name:expr, $f:block) => {{
-        let start = std::time::Instant::now();
-        let result = $f;
-        let end = std::time::Instant::now();
-        log::debug!("{} (took {:?})", $name, end.duration_since(start));
-        result
-    }};
-}
 
 #[allow(unused_macros)]
 macro_rules! time_trace {
