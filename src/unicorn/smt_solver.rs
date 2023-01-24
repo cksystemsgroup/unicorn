f4a01b436ca9ef5a6f066e743b9c5b3742287189
smml1996 <michelle_muroya@hotmail.com>
2023-01-24 23:06:33 +0100

    feat: cover divw node in smt_solver

--- src/unicorn/smt_solver.rs
+++ src/unicorn/smt_solver.rs
@@ -197,6 +197,13 @@ pub mod boolector_impl {
                     let bv_right = self.visit(right).into_bv();
                     bv_left.udiv(&bv_right).into()
                 },
+                Node::Divw { left, right, .. } => {
+                    let bv_left = self.visit(left).into_bv();
+                    let bv_right = self.visit(right).into_bv();
+                    let bv_left32 = bv_left.slice(31, 0);
+                    let bv_right32 = bv_right.slice(31, 0);
+                    bv_left32.sdiv(&bv_right32).sext(32).into()
+                }
                 Node::Rem { left, right, .. } => {
                     let bv_left = self.visit(left).into_bv();
                     let bv_right = self.visit(right).into_bv();
@@ -423,6 +430,13 @@ pub mod z3solver_impl {
                     let z3_left = self.visit(left).as_bv().expect("bv");
                     let z3_right = self.visit(right).as_bv().expect("bv");
                     z3_left.bvudiv(&z3_right).into()
+                }
+                Node::Divw { left, right, .. } => {
+                    let z3_left = self.visit(left).as_bv().expect("bv");
+                    let z3_right = self.visit(right).as_bv().expect("bv");
+                    let z3_left32 = z3_left.extract(31, 0);
+                    let z3_right32 = z3_right.extract(31, 0);
+                    z3_left32.bvsdiv(&z3_right32).sign_ext(32).into()
                 }
                 Node::Rem { left, right, .. } => {
                     let z3_left = self.visit(left).as_bv().expect("bv");
