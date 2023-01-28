d20d13c604bc481c2685367e3d0c739ae478dab1
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-01-29 00:57:09 +0100

    Modeler: Implement divuw

--- src/emulate.rs
+++ src/emulate.rs
@@ -286,6 +286,7 @@ fn execute(state: &mut EmulatorState, instr: Instruction) {
         Instruction::Sraw(rtype) => exec_sraw(state, rtype),
         Instruction::Mulw(rtype) => exec_mulw(state, rtype),
         Instruction::Divw(rtype) => exec_divw(state, rtype),
+        Instruction::Divuw(rtype) => exec_divuw(state, rtype),
         Instruction::Remw(rtype) => exec_remw(state, rtype),
         Instruction::Ecall(_itype) => exec_ecall(state),
         // TODO: Cover all needed instructions here.
@@ -913,6 +914,18 @@ fn exec_divu(state: &mut EmulatorState, rtype: RType) {
     assert!(rs2_value != 0, "check for non-zero divisor");
     let rd_value = rs1_value.wrapping_div(rs2_value);
     trace_rtype(state, "divu", rtype, rd_value);
+    state.set_reg(rtype.rd(), rd_value);
+    state.pc_next();
+}
+
+// rd = s64(rs1{32} /u rs2{32})
+// pc = pc + 4
+fn exec_divuw(state: &mut EmulatorState, rtype: RType) {
+    let rs1_value = state.get_reg(rtype.rs1());
+    let rs2_value = state.get_reg(rtype.rs2());
+    assert!((rs2_value as u32) != 0, "check for non-zero divisor");
+    let rd_value = (rs1_value as u32).wrapping_div(rs2_value as u32) as u64;
+    trace_rtype(state, "divuw", rtype, rd_value);
     state.set_reg(rtype.rd(), rd_value);
     state.pc_next();
 }
