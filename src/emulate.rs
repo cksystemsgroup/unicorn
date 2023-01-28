65fe137900ab6cd7d328268df8f7f679bd3a753e
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2023-01-29 00:56:49 +0100

    Modeler: Implement srlw

--- src/emulate.rs
+++ src/emulate.rs
@@ -282,6 +282,7 @@ fn execute(state: &mut EmulatorState, instr: Instruction) {
         Instruction::Addw(rtype) => exec_addw(state, rtype),
         Instruction::Subw(rtype) => exec_subw(state, rtype),
         Instruction::Sllw(rtype) => exec_sllw(state, rtype),
+        Instruction::Srlw(rtype) => exec_srlw(state, rtype),
         Instruction::Mulw(rtype) => exec_mulw(state, rtype),
         Instruction::Divw(rtype) => exec_divw(state, rtype),
         Instruction::Remw(rtype) => exec_remw(state, rtype),
@@ -761,6 +762,17 @@ fn exec_sllw(state: &mut EmulatorState, rtype: RType) {
     let rs2_value = state.get_reg(rtype.rs2());
     let rd_value = (rs1_value as i32).wrapping_shl(rs2_value as u32) as u64;
     trace_rtype(state, "sllw", rtype, rd_value);
+    state.set_reg(rtype.rd(), rd_value);
+    state.pc_next();
+}
+
+// rd = s64(rs1{32} >>u z32(rs2{6}))
+// pc = pc + 4
+fn exec_srlw(state: &mut EmulatorState, rtype: RType) {
+    let rs1_value = state.get_reg(rtype.rs1());
+    let rs2_value = state.get_reg(rtype.rs2());
+    let rd_value = (rs1_value as u32).wrapping_shr(rs2_value as u32) as u64;
+    trace_rtype(state, "srlw", rtype, rd_value);
     state.set_reg(rtype.rd(), rd_value);
     state.pc_next();
 }
